09db70433ef372b080aa01c5a6fc8ca740bb72c7
Dcoveney22 <156584156+Dcoveney22@users.noreply.github.com>
2025-06-04 17:00:50 +0100

    adjusted topicsSlice as per example

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -1,21 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { useSelector } from "react-redux";
 
-const initialState = { topic: {} };
+const initialState = { topics: {} };
 
 //Step 5
 const topicsSlice = createSlice({
   name: "topics",
   initialState: initialState,
-  reducer: {
+  reducers: {
     addTopic: (state, action) => {
-      const newTopic = {
-        id: "",
-        name: "",
-        icon: "",
-        quizID: []
-      };
-      state.topic.push(newTopic);
+      state.topics[action.payload.id] = action.payload;
+      state.topics[action.payload.id].quizIds = [];
     }
   }
 });
